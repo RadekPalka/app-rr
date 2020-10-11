b7ce05bc78494cf4d40b50bf62e29d184dc0182e
Radosław Pałka <radekpalka@gmail.com>
2020-10-11 18:24:06 +0200

    Updates

---
+++ precache-manifest.e596f2cb6b48574ee2d6388428dfa8e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f8a175a8b18cf6e215d",
+    "url": "/app-rr/static/css/main.87d860ef.chunk.css"
+  },
+  {
+    "revision": "5f8a175a8b18cf6e215d",
+    "url": "/app-rr/static/js/main.4d9b12fd.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "8e34c283b7db7172c355",
+    "url": "/app-rr/static/css/2.c3b71719.chunk.css"
+  },
+  {
+    "revision": "8e34c283b7db7172c355",
+    "url": "/app-rr/static/js/2.35ecda56.chunk.js"
+  },
+  {
+    "revision": "df41df2ecc8950817f4c193f9a39c4f8",
+    "url": "/app-rr/index.html"
+  }
+];
